c1749600f0ef615c82232da1031e2bee43548168
Vladimir Vjatkin <viatkin@volny.cz>
2024-06-08 18:22:16 +0200

    Add project files.

---
+++ 001SimpleSolution/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleAppTEST001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 2)
+            {
+                
+
+                //Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b,
+                //введенные из командной строки, и выводящую результат выполнения на экран.
+
+                int num1 = int.Parse(args[0]);
+                int num2 = int.Parse(args[2]);
+                char znak = char.Parse(args[1]);
+                int rezult = 0;
+                if (num2 == 0 && znak == '/') { 
+                    Console.WriteLine("Divide /0 - that is not possible"); 
+                    return;
+                }
+                switch (znak)
+                {
+                    case '+':
+                        rezult = num1 + num2;
+                        break;
+                    case '-':
+                        rezult = num1-num2;
+                        break;
+                    case '*':
+                        rezult = num1*num2;
+                        break;
+                    case '/':
+                        rezult = num1/num2;
+                        break;
+                    case '%':
+                        rezult = num1%num2;
+                        break;
+                    default:
+                        Console.WriteLine($" {znak} - we don't understand it");
+                        break;
+
+                }
+                Console.WriteLine($"{num1} {znak} {num2} = {rezult}");
+
+            }
+            else Console.WriteLine("We need integer arguments - example 1 + 2");
+        }
+    }
+}
---
+++ 001difficultSolution/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaCalculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = Calculate(args);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.Write($"{args[i]} ");
+            }
+            
+            Console.WriteLine($"= {result}");
+        }
+
+        static double Calculate(string[] args)
+        {
+            List<double> numbers = new List<double>();
+            List<string> operations = new List<string>();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i % 2 == 0)
+                    numbers.Add(double.Parse(args[i]));
+                else
+                    operations.Add(args[i]);
+            }
+
+            for (int i = 0; i < operations.Count; i++)
+            {
+                if (operations[i] == "*")
+                {
+                    numbers[i] = numbers[i] * numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+                    operations.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            for (int i = 0; i < operations.Count; i++)
+            {
+                if (operations[i] == "/")
+                {   if (numbers[i+1]==0)
+                    {
+                        Console.WriteLine($"!!!Divide By Zero - {numbers[i]} / {numbers[i+1]} !!!!\nPlease control your formula in command line\n" +
+                            $"!!! Rezult will be not corrected  !!!! ");
+                        break;
+                    }
+                    numbers[i] = numbers[i] / numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+                    operations.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            double result = numbers[0];
+
+            for (int i = 0; i < operations.Count; i++)
+            {
+                if (operations[i] == "+")
+                    result += numbers[i + 1];
+                else if (operations[i] == "-")
+                    result -= numbers[i + 1];
+            }
+
+            return result;
+        }
+    }
+
+}
+
