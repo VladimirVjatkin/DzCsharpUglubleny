62bd6d2eb4987d8b559ab45bc9313c504a6d2549
Vladimir Vjatkin <viatkin@volny.cz>
2024-08-31 20:53:50 +0200

    dz_Chat001

---
+++ 0001 dz chat/Chat001/Chat.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Chat001
+{
+    internal class Chat
+    {
+        public static void Server(string name)
+        {
+            UdpClient udpClient;
+            IPEndPoint remoteEndPoint;
+            udpClient = new UdpClient(12345);
+            remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Ser_UDP Client wait messages...");
+            while (true)
+            {
+                byte[] receiveBytes = udpClient.Receive(ref remoteEndPoint);
+                string receivedData = Encoding.UTF8.GetString(receiveBytes);
+                try
+                {
+                    var message = Message.FromJson(receivedData);
+                    Console.WriteLine($"Ser_Receive message from {message.FromName} ({message.Date}):");
+                    Console.WriteLine(message.Text);
+                    Console.Write("Type your answer and press Enter: ");
+                    string replyMessage = Console.ReadLine();
+                    var replyMessageJson = new Message()
+                    {
+                        Date = DateTime.Now,
+                        FromName = name,
+                        Text = replyMessage
+                    }.ToJson();
+                    byte[] replyBytes =
+                    Encoding.ASCII.GetBytes(replyMessageJson);
+                    udpClient.Send(replyBytes, replyBytes.Length,
+                    remoteEndPoint);
+                    Console.WriteLine("Response sent.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error during message processing: " +
+                    ex.Message);
+                }
+            }
+        }
+
+
+
+        public static void Client(string name, string ip)
+        {
+            UdpClient udpClient;
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
+            udpClient = new UdpClient();
+            // remoteEndPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("CL_UDP Client wait messages...");
+                    Console.Write("CL_Type your answer and press Enter:  ");
+                    string message = Console.ReadLine();
+                    var messageJson = new Message()
+                    {
+                        Date = DateTime.Now,
+                        FromName = name,
+                        Text = message
+                    }.ToJson();
+                    byte[] replyBytes = Encoding.ASCII.GetBytes(messageJson);
+                    udpClient.Send(replyBytes, replyBytes.Length,
+                    remoteEndPoint);
+                    Console.WriteLine("CL_Response sent.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("CL_Error during message processing: " + ex.Message);
+                }
+                byte[] receiveBytes = udpClient.Receive(ref remoteEndPoint);
+                string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                var messageReceived = Message.FromJson(receivedData);
+                Console.WriteLine($"CL_Message received from {messageReceived.FromName} ({messageReceived.Date}):");
+                Console.WriteLine(messageReceived.Text);
+
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ 0001 dz chat/Chat001/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Chat001
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 1)
+            {
+                Chat.Server(args[0]);
+            }
+            else
+            if (args.Length == 2)
+            {
+                Chat.Client(args[0], args[1]);
+            }
+            else
+            {
+                Console.WriteLine("To start the server, enter nickname as an application startup parameter");
+                Console.WriteLine("To start the client, enter the nickname and IP of the server as application startup parameters");
+            }
+        }
+
+
+    }
+
+
+}
